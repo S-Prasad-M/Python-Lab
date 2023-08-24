507f9bc07dd9bcf95496ab2e33e3a460871c8b80
Shree Prasad M <sprasad2804@gmail.com>
2023-08-24 08:07:42 +0000

    ..

--- Week 5/Program.py
+++ Week 5/Program.py
@@ -1,5 +1,5 @@
-a=int(input("enter a value: "))
-b=int(input("enter b value: "))
+a = int(input("Ënter value for a: ")) 
+b = int(input("Ënter value for b: ")) 
 print(a+b)
 print(a is not b)
 print(a and b)
