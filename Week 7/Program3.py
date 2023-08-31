d7d11af82ab5f7de1137005e7ba66a18317bc021
Shree Prasad M <sprasad2804@gmail.com>
2023-08-31 08:10:35 +0000

    ....

---
+++ Week 7/Program3.py
@@ -0,0 +1,18 @@
+print("1: Sunday\n 2: Monday \n3.Tuesday \n4.Wednesday \n5.Thursday \n6.Friday \n7.Saturday")
+ch = int(input('Enter option to print'))
+if (ch == 1):
+    print("Sunday")
+elif(ch == 2):
+    print("Monday")
+elif(ch == 3):
+    print("Tuesday")
+elif(ch == 4):
+    print("Wednesday")
+elif(ch == 5):
+    print("Thursday")
+elif(ch == 6):
+    print("Friday")
+elif (ch == 7):
+    print("Saturday")
+else:
+    print("Wrong option)")
