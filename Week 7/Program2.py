a2ee5be157abc86a89c7fe58dfb8beea57f98f36
Shree Prasad M <sprasad2804@gmail.com>
2023-08-31 08:02:12 +0000

    ....

---
+++ Week 7/Program2.py
@@ -0,0 +1,9 @@
+a = int(input("Ënter a: "))
+b = int(input("Ënter b: "))
+c = int(input("Ënter c: "))
+if (a>b) and (a>c):
+    print('A is greatest')
+elif (a<b) and (b>c):
+    print('B is greatest')
+else:
+    print('C is greatest')
