24b7ddd375105bacd7c1d6975dc0366624579340
Shree Prasad M <sprasad2804@gmail.com>
2023-09-14 08:27:58 +0000

    ..

---
+++ Week 9/Program2.py
@@ -0,0 +1,8 @@
+import numpy as np 
+T = (1.1,2.2,3.3,4.4)
+arr=np.array([[1.1,2.2,3.3],[4.4,2.2,5.5]]) 
+print("Array is of type:",type(arr)) 
+print("no.of dimensions:",arr.ndim) 
+print("Shape of array:",arr.shape)
+print("Size of array:",arr.size)
+print("Array stores elements of type:",arr.dtype)
