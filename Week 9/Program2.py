91fcdda9da379d6b1c391835aaef4d39ebdf3679
Shree Prasad M <sprasad2804@gmail.com>
2023-09-14 08:34:27 +0000

    ..

--- Week 9/Program2.py
+++ Week 9/Program2.py
@@ -1,8 +1,11 @@
 import numpy as np 
 T = (1.1,2.2,3.3,4.4)
-arr=np.array([[1.1,2.2,3.3],[4.4,2.2,5.5]]) 
-print("Array is of type:",type(arr)) 
-print("no.of dimensions:",arr.ndim) 
-print("Shape of array:",arr.shape)
-print("Size of array:",arr.size)
-print("Array stores elements of type:",arr.dtype)
+L= [[1.1,2.2,3.3],[4.4,2.2,5.5]]
+arr=np.array(L) 
+print("Ärray from list:",arr)
+a = np.zeros((3, 4))
+print("Array with 3x4 zeroes",a)
+arr=np.array(T) 
+print("Ärray from tuple:",arr) 
+array = np.random.rand(3, 4)
+print("2D Array filled with random values : \n", array)
