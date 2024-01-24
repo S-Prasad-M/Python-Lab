ace58514c90e062c1a4e7beb18f4458a4ff25201
Shree Prasad M <sprasad2804@gmail.com>
2024-01-24 04:35:40 +0000

    ...

--- IBS-2/NeedleMan.py
+++ IBS-2/NeedleMan.py
@@ -9,7 +9,12 @@ for i in range(n2):
     matrix[i][0] = i * mismatch
 for i in range(n1):
     matrix[0][i] = i * mismatch
-for i in range(n2):
-    for j in range(n1):
+for i in range(1,n2):
+    for j in range(1,n1):
         diagonal = 0
+        if seq1[i-1] == seq2[j-1]:
+            diagonal += match
+        else:
+            diagonal += mismatch
         
+
