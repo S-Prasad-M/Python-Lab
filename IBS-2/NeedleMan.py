d5e4a82d5d8bf9fbd6d12b2e32f6e05e4e057b52
Shree Prasad M <sprasad2804@gmail.com>
2024-01-24 04:26:12 +0000

    ...

--- IBS-2/NeedleMan.py
+++ IBS-2/NeedleMan.py
@@ -3,9 +3,13 @@ match = 2
 mismatch = -1
 gap = -2
 seq1 = "ATGCA"; n1 = len(seq1)
-seq2 = "AGCA"; n2 = len(seq2)
+seq2 = "AGCAA"; n2 = len(seq2)
 matrix = [[0] * n1 for _ in range(n2)]
 for i in range(n2):
     matrix[i][0] = i * mismatch
 for i in range(n1):
     matrix[0][i] = i * mismatch
+for i in range(n2):
+    for j in range(n1):
+        diagonal = 0
+        
