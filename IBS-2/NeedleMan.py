347337635d4910ebfa72ec0c9aecd395a92f4905
Shree Prasad M <sprasad2804@gmail.com>
2024-01-24 06:27:55 +0000

    ....

--- IBS-2/NeedleMan.py
+++ IBS-2/NeedleMan.py
@@ -3,22 +3,22 @@ match = 2
 mismatch = -1
 gap = -2
 seq1 = "ATGCA"; n1 = len(seq1)+1
-seq2 = "AGCAA"; n2 = len(seq2)+1
+seq2 = "ATGCA"; n2 = len(seq2)+1
 matrix = [[0] * n1 for _ in range(n2)]
 for i in range(n2):
     matrix[i][0] = i * gap
 for i in range(n1):
     matrix[0][i] = i * gap
 
-for i in range(1,n2):
-    for j in range(1,n1):
+for i in range(1,n1):
+    for j in range(1,n2):
         diagonal = matrix[i-1][j-1]
-        if seq1[i-1] == seq2[j-1]:
+        if seq1[j-1] == seq2[i-1]:
             diagonal += match
         else:
             diagonal += mismatch
-        left = matrix[i][j-1] + gap
-        up = matrix[i - 1][j] + gap
+        left = matrix[i-1][j] + gap
+        up = matrix[i][j-1] + gap
         matrix[i][j] = max(diagonal, up, left)
 
 for i in range(n1):
@@ -32,6 +32,9 @@ while i!=0 and j!=0:
     if seq1[i-1] == seq2[j-1]:
         i-=1; j-=1
         s1+=seq1[i]; s2+=seq2[j]
+print(s1)
+print(s2)
+
 
 
 
