1b6240e7f274c56e7095036619ff7eec617e60a6
Shree Prasad M <sprasad2804@gmail.com>
2024-01-31 05:45:24 +0000

    ...

--- IBS-2/NeedleManWunsch.py
+++ IBS-2/NeedleManWunsch.py
@@ -2,24 +2,16 @@ matrix = []
 match = 2
 mismatch = -1
 gap = -2
-seq1 = "ATGTA"
-seq2 = "AGCA"
-
-n1 = len(seq1) + 1
-n2 = len(seq2) + 1
-
-# Determine the longer sequence length
+seq1 = "ATGCA"
+seq2 = "ATTGGCA"
+n1 = len(seq1) + 1; n2 = len(seq2) + 1
 max_len = max(n1, n2)
-
-# Initialize the matrix with appropriate dimensions
 matrix = [[0] * max_len for _ in range(max_len)]
-
-# Fill the matrix with appropriate initial values
 for i in range(max_len):
     matrix[i][0] = i * gap
     matrix[0][i] = i * gap
 
-# Fill the matrix following the algorithm
+# Matrix filling
 for i in range(1, n2):
     for j in range(1, n1):
         diagonal = matrix[i - 1][j - 1]
@@ -30,13 +22,12 @@ for i in range(1, n2):
         left = matrix[i - 1][j] + gap
         up = matrix[i][j - 1] + gap
         matrix[i][j] = max(diagonal, up, left)
+for i in matrix:
+    print(i)
+# print(matrix)
 
-# Find the alignment
-i = n2 - 1
-j = n1 - 1
-s1 = ""
-s2 = ""
-
+i = n2 - 1; j = n1 - 1
+s1 = ""; s2 = ""
 while i > 0 or j > 0:
     if i > 0 and j > 0 and seq1[j - 1] == seq2[i - 1]:
         s1 = seq1[j - 1] + s1
@@ -54,3 +45,14 @@ while i > 0 or j > 0:
 
 print("Sequence 1:", s1)
 print("Sequence 2:", s2)
+
+score = 0
+for i in range(len(s1)):
+    if s1[i] == s2[i]:
+        score += 2
+    elif s1[i]=="_" or s2[i]=="_":
+        score -= 2
+    elif s1[i] != s2[i]:
+        score -= 1
+    
+print("Score:",score)
