3aff162e937c54e0de5e5e2d114c04b38f227fa6
Shree Prasad M <sprasad2804@gmail.com>
2024-02-01 23:00:24 +0530

    Traceback updated

--- IBS-2/DotPlot.py
+++ IBS-2/DotPlot.py
@@ -11,12 +11,12 @@ for i in matrix:
 
 i = n1 - 1; j = n2 - 1
 s1 = ""; s2 = ""
-while i > 0 or j > 0:
-    if i > 0 and j > 0 and seq1[i - 1] == seq2[j - 1]:
+while i >= 0 or j >= 0:
+    if i >= 0 and j >= 0 and seq1[i - 1] == seq2[j - 1]:
         s1 = seq1[i] + s1
         s2 = seq2[j] + s2
         i -= 1; j -= 1
-    elif i > 0 and (j == 0 or matrix[i][j] == matrix[i - 1][j] -2):
+    elif i >= 0 and (j == 0 or matrix[i][j] == matrix[i - 1][j] -2):
         s1 = '_' + s1
         s2 = seq2[j - 1] + s2
         i -= 1
